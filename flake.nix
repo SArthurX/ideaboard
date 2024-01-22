66d0280c0d741ca3de8cc7a1d413144a80e097ac
kryr34 <uhamia@protonmail.com>
2024-01-23 05:29:55 +0800

    ♻️ use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,49 +1,32 @@
 {
-
-  description = "Example Haskell development environment for Zero to Nix";
+  description = "Ideaborad development enviroment";
   nixConfig.bash-prompt = "[nix]\\e\[38;5;172mλ \\e\[m";
 
-  # Flake inputs
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
     let
-      allSystems = [
-        "x86_64-linux" "aarch64-linux"
-        "x86_64-darwin" "aarch64-darwin"
-      ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f {
-          system = system;
-          pkgs = import nixpkgs { inherit system; };
-        });
-    in {
-      # equal to packages.default or package.<system>.default idrk
-      packages = forAllSystems ({ pkgs, ... }: {
-        default = {
-          postgrest = pkgs.postgrest;
-        };
-      });
-      checks = self.packages;
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          postgrest
+          cowsay
+          docker
+          docker-compose
+          jq
+          postgresql # for psql
+        ];
 
-      devShells = forAllSystems ({ system, pkgs }: {
-        default = pkgs.mkShell {
-          nativeBuildInputs = [
-            self.packages.${system}.default.postgrest
-            pkgs.cowsay
-            pkgs.docker
-            pkgs.docker-compose
-            pkgs.jq
-            pkgs.postgresql # for psql
-          ];
-          shellHook = ''
-            postgrest --version
-          '';
-        };
-      });
-    };
+        shellHook = ''
+          postgrest --version
+        '';
+      };
+    }
+  );
 }
