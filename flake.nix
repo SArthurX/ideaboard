095bd1e10b8859ac7aa6cf0e19017c7bf4caca2d
SArthurX <sxa05z@gmail.com>
2024-02-10 23:17:07 +0800

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,29 @@
 {
-  description = "Ideaborad development enviroment";
-  nixConfig.bash-prompt = "[nix]\\e\[38;5;172mλ \\e\[m";
+  description = "lol";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          postgrest
-          cowsay
-          docker
-          docker-compose
-          jq
-          postgresql # for psql
-        ];
-
-        shellHook = ''
-          postgrest --version
-        '';
-      };
-    }
-  );
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+        isDarwin = system == "aarch64-darwin";
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            postgresql
+            docker
+            docker-compose
+            jq
+          ];
+        };
+      });
 }
+
